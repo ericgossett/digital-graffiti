881bb43bd5a877cfaabf5eeb7df8a5dcb7c0ed14
jb325 <john.bralich@duke.edu>
2017-11-08 11:07:10 -0500

    Finished available collection view demo

---
+++ NoahsProject/AvailableCollectionCell.swift
@@ -0,0 +1,16 @@
+//
+//  AvailableCollectionCell.swift
+//  NoahsProject
+//
+//  Created by ece on 11/7/17.
+//  Copyright © 2017 Duke University. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AvailableCollectionCell:UICollectionViewCell{
+    @IBOutlet weak var cellIm: UIImageView!
+    @IBOutlet weak var cellLab: UILabel!
+    @IBOutlet weak var cellCheck: UIImageView!
+}
