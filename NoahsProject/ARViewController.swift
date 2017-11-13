8d6f071624291198dbdfcb6e24be8cbcb41db0c5
yw215 <yw215@duke.edu>
2017-11-12 22:49:51 -0500

    Changed the default ARKit viewController to the ARViewController for later easier usage. added a round button - draw button and several debug things for ARView.

---
+++ NoahsProject/ARViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ARViewController.swift
+//  NoahsProject
+//
+//  Created by YUNHAN WANG on 11/12/17.
+//  Copyright © 2017 Duke University. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ARViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet weak var drawButton: RoundButton!
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.showsStatistics = true
+        self.sceneView.session.run(configuration)
+        self.sceneView.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
