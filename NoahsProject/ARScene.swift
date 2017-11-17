494250c8072fabdef82bf620cffd1bbfe4874c73
yw215 <yw215@duke.edu>
2017-11-17 16:48:05 -0500

    removed previous storyboard settings. Added vision functionality.

---
+++ NoahsProject/ARScene.swift
@@ -0,0 +1,15 @@
+//
+//  ARScene.swift
+//  NoahsProject
+//
+//  Created by YUNHAN WANG on 11/17/17.
+//  Copyright © 2017 Duke University. All rights reserved.
+//
+
+import UIKit
+import ARKit
+import Vision
+
+class ARScene: SCNScene {
+    
+}
