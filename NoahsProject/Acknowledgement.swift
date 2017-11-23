fc188c4f9d2e3797eeea1d74e3e609f6faab4a3d
yw215 <yw215@duke.edu>
2017-11-23 17:46:17 -0500

    found 5 visible models, added an acknowledgement file for thanking all model contributers from google blocks

---
+++ NoahsProject/Acknowledgement.swift
@@ -0,0 +1,15 @@
+//
+//  Acknowledgement.swift
+//  NoahsProject
+//
+//  Created by YUNHAN WANG on 11/23/17.
+//  Copyright © 2017 Duke University. All rights reserved.
+//
+
+// This file is to thank all the model creaters in the Google Blocks
+
+
+// Android Robot by Naomi Chen
+// Earth by Robert Mirabelle
+// Butterfly by Poly by Google
+// Airplane by Poly by Google
